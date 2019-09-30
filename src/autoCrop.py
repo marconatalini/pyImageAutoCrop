7883ec40008c8185ec5c15d817e17e46befaa9b2
Marco Natalini <marconatalini.75@gmail.com>
2019-09-30 22:31:26 +0200

    get box for crop

--- src/autoCrop.py
+++ src/autoCrop.py
@@ -1,3 +1,5 @@
+
+
 '''
 Created on 30 ago 2019
 
@@ -8,6 +10,7 @@ from PIL import Image
 import os
 
 JPG_SOURCE_PATH = r's:\Disegni_listini\Europrofili'
+JPG_SOURCE_PATH = r'c:\Users\Marco\symfony4\eurolist\public\jpgs'
 JPG_DESTINATION_PATH = r'c:\Users\marco\Documents\Stampe\jpgs'
 
 def isPortrait(size):
@@ -15,6 +18,25 @@ def isPortrait(size):
         return True
     else:
         return False
+    
+def isWhite(pixel):
+    if pixel == (255,255,255):
+        return True
+    else:
+        return False
+    
+def getBoundBox(im): #image object    
+    left,upper,right,lower = im.size[0],im.size[1],0,0
+    px = im.load()
+    for x in range(im.size[0]):
+        for y in range(im.size[1]):
+            if not(isWhite(px[x,y])):
+                if x < left:  left = x
+                if y < upper: upper = y
+                if x > right: right = x
+                if y > lower: lower = y
+    box = (left,upper,right,lower) #left,upper,right,lower
+    return box
 
 def autoCrop():    
     with os.scandir(JPG_SOURCE_PATH) as jpgs:
@@ -32,4 +54,9 @@ def autoCrop():
 #                         im2.save(os.path.join(JPG_DESTINATION_PATH, entry.name))
 
 if __name__ == '__main__':
-    autoCrop()
+    imfile = r"c:\Users\Marco\symfony4\eurolist\public\jpgs\16000720.jpg"
+    im = Image.open(imfile)
+    b = getBoundBox(im)
+    im_crop = im.crop(b)
+    im_crop.save(os.path.join(JPG_DESTINATION_PATH, 'prova.jpg'))
+    im
